5b1306781fa26845e7d4a11d18eb9db62680eaeb
jeff-h <jeffvh@outlook.com>
2020-05-01 20:03:35 +1000

    separate seed of data to it own file and add querying

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,4 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -11,14 +10,21 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Jeff',
-        age: 28
-    }, (error, result) => {
+    db.collection('users').findOne({ name: "Jeff", age: 28 }, (error, user) => {
         if (error) {
-            return console.log('Unable to insert user')
+            return console.log('Unable to fetch')
         }
 
-        console.log(result.ops)
+        console.log(user)
+    })
+
+    // find() by itself returns a cursor rather than object or list of data.
+    // Follow-up methods will determine what to do with cursor
+    db.collection('users').find({ age: 27 }).toArray((error, users) => {
+        console.log(users)
+    })
+
+    db.collection('users').find({ age: 27 }).count((error, users) => {
+        console.log(users)
     })
 })
---
+++ seedData.js
@@ -0,0 +1,57 @@
+const { MongoClient } = require('mongodb');
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        console.log("Unable to connect to database!")
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Jeff',
+        age: 28
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert user')
+        }
+
+        console.log(result.ops)
+    })
+
+    db.collection('users').insertMany([
+        {
+            name: "Jo",
+            age: 34
+        },
+        {
+            name: 'Kat',
+            age: 32
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents')
+        }
+
+        console.log(result.ops)
+    })
+
+    db.collection('tasks').insertMany([
+        {
+            description: "Complete Course",
+            completed: false
+        },
+        {
+            desciption: 'Start reading book',
+            completed: true
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents')
+        }
+
+        console.log(result.ops)
+    })
+})
