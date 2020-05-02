7598386989a6d07b7be570df9ad3d00e6657ad06
jeff-h <jeffvh@outlook.com>
2020-05-02 17:50:52 +1000

    ensure tasks dealt with are for authorised user only

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,8 +20,9 @@ router.post('/tasks', auth, async (req, res) => {
 
 router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        // const tasks = await Task.find({ owner: req.user._id })
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
     }
@@ -31,7 +32,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: req.user._id })
         if (!task) {
             return res.status(404).send({ error: "Task not found" })
         }
@@ -51,27 +52,28 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(req.params.id)
-
-        updates.forEach(update =>  task[update] = req.body[update])
-        await task.save();
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
         if (!task) {
             return res.status(404).send({ error: "Task not found" });
         }
+
+        updates.forEach(update =>  task[update] = req.body[update])
+        await task.save();
         res.send(task);
     } catch (e) {
         res.status(500).send({ error: e.message });
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
         if (!task) {
             res.status(404).send({ error: "Task not found" })
+        } else {
+            res.send(task)
         }
-        res.send(task)
     } catch (e) {
         res.status(500).send({ error: e.message });
     }
