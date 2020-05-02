0258a790dc84d8bf4bd1cee77c8f21d6e194b5a0
jeff-h <jeffvh@outlook.com>
2020-05-02 15:03:32 +1000

    add hashing to seed data

--- src/db/seed/seedData.js
+++ src/db/seed/seedData.js
@@ -1,10 +1,15 @@
 // run this file in terminal to seed data into MongoDB --> node seedData.js
 const { MongoClient, ObjectID } = require('mongodb');
+const bcrypt = require('bcrypt')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager-api'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+const getHashedPassword = async (pw) => {
+    return await bcrypt.hash(pw, 8)
+}
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, async (error, client) => {
     if (error) {
         console.log("Unable to connect to database!")
     }
@@ -16,7 +21,7 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         name: 'Jeff',
         age: 28,
         email: "jeff@mail.com",
-        password: "Testpw123!"
+        password: await getHashedPassword("Testpw123!")
     }, (error, result) => {
         if (error) {
             return console.log('Unable to insert user')
@@ -30,13 +35,13 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
             name: "Jo",
             age: 34,
             email: "jo@fake.com",
-            password: "Testpw111!"
+            password: await getHashedPassword("Testpw111!")
         },
         {
             name: 'Kat',
             age: 32,
             email: "kat@example.com",
-            password: "Testpw222!"
+            password: await getHashedPassword("Testpw222!")
         }
     ], (error, result) => {
         if (error) {
