51b17dee9a1cb7e6a2bdb0ca9dbeb0a22f5546b4
jeff-h <jeffvh@outlook.com>
2020-05-03 09:10:24 +1000

    save image as buffer into db and add delete avatar endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -93,7 +93,6 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars',
     limits: {
         // Number in bytes. below is 1MB
         fileSize: 1000000
@@ -107,10 +106,22 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.send()
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    try {
+        req.user.avatar = undefined
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send({ error: e.message });
+    }
 })
 
 module.exports = router
