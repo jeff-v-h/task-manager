4095bf40678052e1fade60c47425c278485e2c87
jeff-h <jeffvh@outlook.com>
2020-05-02 12:31:27 +1000

    move and update seed data file

--- src/db/seed/seedData.js
+++ src/db/seed/seedData.js
@@ -1,7 +1,8 @@
+// run this file in terminal to seed data into MongoDB --> node seedData.js
 const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
+const databaseName = 'task-manager-api'
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -11,8 +12,11 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     const db = client.db(databaseName)
 
     db.collection('users').insertOne({
+        _id: new ObjectID('5eacb3c9c5083154201e150e'),
         name: 'Jeff',
-        age: 28
+        age: 28,
+        email: "jeff@mail.com",
+        password: "Testpw123!"
     }, (error, result) => {
         if (error) {
             return console.log('Unable to insert user')
@@ -23,13 +27,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     db.collection('users').insertMany([
         {
-            _id: new ObjectID("5eabde86c974833f54f8a522"),
             name: "Jo",
-            age: 34
+            age: 34,
+            email: "jo@fake.com",
+            password: "Testpw111!"
         },
         {
             name: 'Kat',
-            age: 32
+            age: 32,
+            email: "kat@example.com",
+            password: "Testpw222!"
         }
     ], (error, result) => {
         if (error) {
