215ccb6639e775bd9217678d1b6d5369f497c77b
jeff-h <jeffvh@outlook.com>
2020-05-03 17:32:17 +1000

    refactor db setup for tests into separate file to be used for task tests also

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,31 +1,19 @@
 const request = require('supertest')
-const jwt = require('jsonwebtoken')
-const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const { userOne, userOneId, setupDatabase } = require('./fixtures/db')
 
-const userOneId = new mongoose.Types.ObjectId()
-const userOne = {
-    _id: userOneId,
-    name: "Michael Jordan",
-    email: 'likemike@example.com',
-    password: '123dunkit!!',
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
-    }]
-}
-
-beforeEach(async () => {
-    await User.deleteMany()
-    await new User(userOne).save()
-})
+beforeEach(setupDatabase)
 
 test('Should signup a new user', async () => {
-    const response = await request(app).post('/users').send({
-        name: 'Jeff',
-        email: 'jeffvh@outlook.com',
-        password: 'MyPass123!'
-    }).expect(201)
+    const response = await request(app)
+        .post('/users')
+        .send({
+            name: 'Jeff',
+            email: 'jeffvh@outlook.com',
+            password: 'MyPass123!'
+        })
+        .expect(201)
 
     // Assert user is added to db
     const user = await User.findById(response.body.user._id)
@@ -43,10 +31,13 @@ test('Should signup a new user', async () => {
 })
 
 test('Should login existing user', async () => {
-    const response = await request(app).post('/users/login').send({
-        email: userOne.email,
-        password: userOne.password
-    }).expect(200);
+    const response = await request(app)
+        .post('/users/login')
+        .send({
+            email: userOne.email,
+            password: userOne.password
+        })
+        .expect(200);
 
     // Assert token is saved in db
     const user = await User.findById(userOneId)
@@ -54,15 +45,19 @@ test('Should login existing user', async () => {
 })
 
 test('Should not login non-existing user', async (done) => {
-    await request(app).post('/users/login').send({
-        email: 'notexist@heyhey.com',
-        password: 'none999$'
-    }).expect(400);
+    await request(app)
+        .post('/users/login')
+        .send({
+            email: 'notexist@heyhey.com',
+            password: 'none999$'
+        })
+        .expect(400);
     done()
 })
 
 test('Should get profile for user', async (done) => {
-    await request(app).get('/users/me')
+    await request(app)
+        .get('/users/me')
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
@@ -70,14 +65,16 @@ test('Should get profile for user', async (done) => {
 })
 
 test('Should not get profile for unauthenticated user', async (done) => {
-    await request(app).get('/users/me')
+    await request(app)
+        .get('/users/me')
         .send()
         .expect(401)
     done()
 })
 
 test('Should delete account for user', async () => {
-    await request(app).delete('/users/me')
+    await request(app)
+        .delete('/users/me')
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
@@ -88,7 +85,8 @@ test('Should delete account for user', async () => {
 })
 
 test('Should not delete account for unauthenticated user', async (done) => {
-    await request(app).delete('/users/me')
+    await request(app)
+        .delete('/users/me')
         .send()
         .expect(401)
     done()
