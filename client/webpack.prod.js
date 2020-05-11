533a343b2712035ccd49e848bbf0326f9c83ecb1
jeff-h <jeffvh@outlook.com>
2020-05-11 18:24:44 +1000

    ensure frontend builds

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,6 +1,7 @@
 const common = require("./webpack.common.js");
 const merge = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = function(env) {
     return merge(common(env), {
